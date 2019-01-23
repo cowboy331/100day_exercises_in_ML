a501008e0a6e7e412798a688dbf4b422a311cf3d
cowboy331 <cowboy331@gmail.com>
2019-01-23 22:26:10 +0800

    '完成d2'

--- d2.py
+++ d2.py
@@ -1,5 +1,31 @@
-#encoding: UTF-8
-# 1 importing the libraries
+# encoding: UTF-8
+# 1 data preprocessing
 import pandas as pd
 import numpy as np
-import matplotlib as 
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('../datasets/studentscores.csv')
+X = dataset.iloc[:, :1].values
+Y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1 / 4, random_state=0)
+
+# 2 Fitting Simple Linear Regression Mode training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor = regressor.fit(X_train, Y_train)
+
+# 3 Predecting the Result
+Y_pred = regressor.predict(X_test)
+
+# 4 Visualization
+plt.scatter(X_train, Y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+
+plt.scatter(X_test, Y_test, color='red')
+plt.plot(X_test, regressor.predict(X_test), color='blue')
+
+plt.show()
